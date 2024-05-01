612a4575e01d593d834ce114bfeda40811c40b6d
BachToanMy <btmy1314@gmail.com>
2024-05-01 22:17:48 +0700

    fix fliter

--- src/sections/censorship/Events/index.jsx
+++ src/sections/censorship/Events/index.jsx
@@ -9,7 +9,7 @@ export default function EventCenSorSection() {
   const [totalPage, setTotalPage,] = useState(0);
   const [status, setStatus,] = useState('all');
   const [approval, setApproval,] = useState('all');
-  const [total, setTotal, ] = useState(0);
+  // const [total, setTotal, ] = useState(0);
   useEffect(() => {
     getEvents();
   }, [currentPage, status, approval, ]);
@@ -26,7 +26,7 @@ export default function EventCenSorSection() {
       .then((data) => {
         setEvent(data?.data);
         setTotalPage(data?.meta?.total_pages);
-        setTotal(data?.meta?.total);
+        // setTotal(data?.meta?.total);
       })
       .catch((error) => console.log(error));
   };
@@ -117,7 +117,7 @@ export default function EventCenSorSection() {
   return (
     <div id={'ProductSection'}>
       <div>
-        <HeaderBar total={total} chuaduyet={chuaduyet} daduyet={daduyet} tuchoi={tuchoi} baocao={baocao} />
+        <HeaderBar total={chuaduyet+daduyet+tuchoi+baocao} chuaduyet={chuaduyet} daduyet={daduyet} tuchoi={tuchoi} baocao={baocao} />
         <Filter setApproval={setApproval} setStatus={setStatus} status={status} approval={approval}/>
         <EventTable
           events={events}
--- src/sections/censorship/Posts/index.jsx
+++ src/sections/censorship/Posts/index.jsx
@@ -12,10 +12,34 @@ export default function PostCencorSection() {
   const [gender, setGender,] = useState('all');
   const [total, setTotal,] = useState(0);
   useEffect(() => {
-    getProducts();
+    if(degree!='all'||gender!='all'||total!='all'){
+      getProductsFilter();
+    }else{
+      getProducts();
+    } 
+    reload();
   }, [currentPage, size, gender, degree,]);
 
   const getProducts = () => {
+    fetch(
+      `${process.env.REACT_APP_HOST_IP}/products/?page=${currentPage}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('access')}`,
+          Accept: 'application/json',
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setProduct(data?.data);
+        setTotalPage(data?.meta?.total_pages);
+        setTotal(data?.meta?.total);
+      })
+      .catch((error) => console.log(error));
+  };
+  const getProductsFilter = () => {
     fetch(
       `${process.env.REACT_APP_HOST_IP}/products/?page=${currentPage}&degree=${degree}&gender=${gender}&size=${size}&limit=12`,
       {
@@ -30,7 +54,6 @@ export default function PostCencorSection() {
       .then((data) => {
         setProduct(data?.data);
         setTotalPage(data?.meta?.total_pages);
-        setTotal(data?.meta?.total);
       })
       .catch((error) => console.log(error));
   };
@@ -115,7 +138,7 @@ export default function PostCencorSection() {
   return (
     <div id={'ProductSection'}>
       <div>
-        <HeaderBar total={total} tuchoi={tuchoi} baocao={baocao} chuaduyet={chuaduyet} daduyet={daduyet} />
+        <HeaderBar total={tuchoi+daduyet+chuaduyet+baocao} tuchoi={tuchoi} baocao={baocao} chuaduyet={chuaduyet} daduyet={daduyet} />
         <Filter
           gender={gender}
           setGender={setGender}
